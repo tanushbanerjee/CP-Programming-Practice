ed0af754995d0059190cdb3bcfbb8d8620d5d9fb
Tanush Banerjee <samihanbanerjee026@gmail.com>
2020-11-08 21:47:33 +0530

    ADDED MORE SOLUTIONS

---
+++ CSESProblemSet/SortingAndSearching/SubarraySumI/sol.cpp
@@ -0,0 +1,48 @@
+/*******************************************************************************
+Author:  Tanush Banerjee 
+Email:  samihanbanerjee026@gmail.com
+CodeForces Username: samihanbanerjee026
+CodeChef Username: tanushbanerjee
+ *******************************Macros And Includes******************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout << #x << " " << x << endl;
+#define fo(i, n) for(i=0; i < n; ++i)
+#define Fo(i, k, n) for(i=k; i<n; ++i)
+#define rv(n, a) for(int i = 0; i < n; ++i) {cin >> a[i];}
+#define max(i, j) (i < j) ? cout<<1:cout<<0;
+#define pb(x) push_back(x);
+using ll = long long;
+using l = long;
+#define fastio ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define ttl int tt;cin>>tt;while(tt--)
+ 
+
+int main()
+{
+	fastio;
+	ll n, x;
+	cin >> n >> x;
+
+	vector<int> a(n);
+	
+	rv(n, a);
+	
+	ll ans = 0;
+
+	ll pref = 0;
+	unordered_map<ll, ll> countPref;
+	countPref[pref]++;
+
+	for(int i = 0; i < n; ++i)
+	{
+		pref += a[i];
+		int need = pref - x;
+
+		ans += countPref[need];
+		countPref[pref]++;
+	}
+	cout << ans;
+	return 0;
+}
